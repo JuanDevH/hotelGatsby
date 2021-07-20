539355e0970d0643263c511c6f4dc1b36197608d
Juan Hernández <hajjose7@gmail.com>
2021-07-19 21:45:49 -0400

    Agregando CSS a la Navegación

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    `gatsby-plugin-emotion`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
